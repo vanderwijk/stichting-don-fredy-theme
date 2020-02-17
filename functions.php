4d37de76f837a73525c342ddcfd0933eefc44c93
Johan van der Wijk <johan@vanderwijk.nl>
2020-02-17 12:07:23 +0100

    Style

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 add_action( 'wp_enqueue_scripts', 'sdf_enqueue_styles' );
 function sdf_enqueue_styles() {
  
-    $parent_style = 'charity-help-lite-basic-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $parent_style = 'charity-help-lite-basic-style-css'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
  
     wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style',
